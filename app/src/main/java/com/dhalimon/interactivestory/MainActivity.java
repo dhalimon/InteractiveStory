bcb9fbd5e73ccd798c8d84144efd33b8a90687f5
dhalimon <Rome@206>
2020-04-24 06:31:08 -0400

    friday

---
+++ app/src/main/java/com/dhalimon/interactivestory/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dhalimon.interactivestory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
